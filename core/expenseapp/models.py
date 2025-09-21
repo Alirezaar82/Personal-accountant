67cfd19d01bc563fbbcb71b78e02956ed36b808d
Alirezaar82 <alirezaalireza82617@gmail.com>
2025-09-21 13:05:04 +0330

    Add expense category and limit models, refactor expense model

--- core/expenseapp/models.py
+++ core/expenseapp/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
-class ExpenseManageModel(models.Model):
+class ExpenseCategoryModel(models.Model):
     user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    limit_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ExpenseModel(models.Model):
+    user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    category = models.ForeignKey(ExpenseCategoryModel, on_delete=models.CASCADE)
     totoal_amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
     date = models.DateField(auto_now_add=True)
@@ -9,4 +20,11 @@ class ExpenseManageModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class ExpenseCategoryModel()
+
+class ExpenseLimitModel(models.Model):
+    user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    limit_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
