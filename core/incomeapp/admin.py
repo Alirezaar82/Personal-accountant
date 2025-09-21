c080cfd918cc5f451268b7dedd01ff690ce0f9d1
Alirezaar82 <alirezaalireza82617@gmail.com>
2025-09-21 14:02:13 +0330

    Register income models in Django admin

--- core/incomeapp/admin.py
+++ core/incomeapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import IncomeCategoryModel, IncomeModel
+
+
+@admin.register(IncomeCategoryModel)
+class IncomeCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'category_name', 'limit_amount', 'created_at', 'updated_at')
+    search_fields = ('category_name', 'user__username')
+    list_filter = ('created_at', 'updated_at')
+    ordering = ('-created_at',)
+
+
+@admin.register(IncomeModel)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'category', 'totoal_amount', 'date', 'created_at', 'updated_at')
+    search_fields = ('category__category_name', 'user__username')
+    list_filter = ('date', 'created_at', 'updated_at')
+    ordering = ('-created_at',)
+
